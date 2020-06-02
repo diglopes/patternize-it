2fad5db8e682cd69a7a577a08bf44fc007ac9c6d
Diego Lopes <diego-chez@hotmail.com>
2020-06-02 09:21:46 -0300

    chore(build): create html template and inject assets on it

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -7,6 +8,12 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./main.html",
+      hash: "true",
+    }),
+  ],
   module: {
     rules: [
       {
