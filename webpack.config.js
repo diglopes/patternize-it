9112f03f5262bd5b3a3617cc461f56ac5327fbfc
Diego Lopes <diego-chez@hotmail.com>
2020-06-02 09:57:49 -0300

    chore(build): extract css from js and compile it into a sepated file

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin");
+
+const prodPlugins = [];
+if (process.env.NODE_ENV === "production") {
+  prodPlugins.push(
+    new OptimizeCssPlugin({
+      cssProcessor: require("cssnano"),
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true,
+        },
+      },
+      canPrint: true,
+    })
+  );
+}
 
 module.exports = {
-  mode: "production",
+  mode: process.env.NODE_ENV || "development",
   entry: "./src/app.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    ...prodPlugins,
     new HtmlWebpackPlugin({
       template: "./main.html",
       hash: "true",
     }),
+    new ExtractTextWebpackPlugin("styles.css"),
   ],
   module: {
     rules: [
@@ -25,6 +44,13 @@ module.exports = {
             presets: ["@babel/preset-env"],
           },
         },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextWebpackPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader",
+        }),
       },
     ],
   },
